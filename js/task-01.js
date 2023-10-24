d724232fd501cba47660df042b2e43ef3c4c0e5f
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2023-10-24 12:25:39 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,7 @@
-const categories = document.querySelector("#categories").children.length;
-console.log(`Number of categories: ${categories}`);
+const categories = document.querySelector("#categories");
+console.log(`Number of categories: ${categories.children.length}`);
 
-const items = document.querySelectorAll(".item").forEach((element) => {
+const items = [...categories.children].forEach((element) => {
   console.log(`Category: ${element.firstElementChild.textContent}`);
   console.log(`Elements: ${element.lastElementChild.children.length}`);
 });
