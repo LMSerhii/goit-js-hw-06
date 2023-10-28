66ba52b2dcbd7dffe23101449662dae2c22ed57a
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2023-10-28 08:33:48 +0300

    task-9 done

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorEl = document.querySelector(".color");
+const changeColorEl = document.querySelector(".change-color");
+const bodyEl = document.querySelector("body");
+
+changeColorEl.addEventListener("click", changeColor);
+
+function changeColor() {
+  const color = getRandomHexColor();
+
+  bodyEl.style.backgroundColor = color;
+  colorEl.textContent = color;
+}
