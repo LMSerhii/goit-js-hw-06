60c42b4536b7fd3550f52adedb462950b74630b0
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2023-10-28 20:35:53 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,8 +1,8 @@
 const remoteControl = document.querySelector("#font-size-control");
 const getText = document.querySelector("#text");
 
-remoteControl.addEventListener("input", getValue);
+remoteControl.addEventListener("input", onRemoteControlEvent);
 
-function getValue(event) {
+function onRemoteControlEvent(event) {
   getText.style.fontSize = `${event.currentTarget.value}px`;
 }
