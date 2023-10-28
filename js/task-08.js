13da7252bb27090eecd984a00b0bc2bd1ce87dc6
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2023-10-28 08:07:04 +0300

    task-8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const formElement = document.querySelector(".login-form");
 
+formElement.addEventListener("submit", getData);
+
+function getData(event) {
+  event.preventDefault();
+
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if ((email === "") | (password === "")) {
+    alert("Всі поля повинні бути заповнені");
+  } else {
+    const user = {
+      email,
+      password,
+    };
+
+    console.log(user);
+  }
+
+  form.reset();
+}
