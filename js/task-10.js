ba2287815e97a23bc892379107c8a70cb70a0cbf
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2023-10-30 14:34:15 +0200

    task-10 done

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,45 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const controls = document.querySelector("#controls");
+const inputElement = controls.children[0];
+const createBtn = controls.children[1];
+const destroyBtn = controls.children[2];
+const boxes = controls.nextElementSibling;
+
+createBtn.addEventListener("click", onCreateBtn);
+destroyBtn.addEventListener("click", onDestroyBtn);
+
+function onCreateBtn() {
+  destroyBoxes();
+  createBoxes(inputElement.value);
+}
+
+function onDestroyBtn() {
+  destroyBoxes();
+}
+
+function createBoxes(amount) {
+  const MAX_ELEMENTS = amount;
+  let size = 30;
+  let iterCounter = 0;
+
+  while (iterCounter < MAX_ELEMENTS) {
+    boxes.insertAdjacentHTML(
+      "beforeend",
+      `<div style="
+      background-color: ${getRandomHexColor()}; 
+      width: ${size}px; 
+      height: ${size}px">
+      </div>`
+    );
+
+    size += 10;
+    iterCounter += 1;
+  }
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
