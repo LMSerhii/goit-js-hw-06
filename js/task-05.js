96fa199cd83cc8bb7d39acb25e9f432bd083cd72
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2023-11-01 10:34:52 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,8 +1,14 @@
 const nameInput = document.querySelector("#name-input");
 const nameOutput = document.querySelector("#name-output");
+const nameText = nameOutput.textContent;
 
 nameInput.addEventListener("input", onNameInput);
 
 function onNameInput(event) {
+  console.log(nameText);
   nameOutput.textContent = event.currentTarget.value;
+
+  if (event.currentTarget.value === "") {
+    nameOutput.textContent = nameText;
+  }
 }
